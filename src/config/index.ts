d935a05177c65ea3ba357f7ac1f1c9ba7f4d6e7f
luster02 <bombolti_02@hotmail.com>
2020-06-14 22:21:08 -0500

    [ADD]: cludinary config

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,5 +6,9 @@ if (process.env.NODE_ENV !== 'PRODUCTION') {
 
 export default {
     PORT: process.env.PORT,
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
+    CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+    CLOUDINARY_KEY: process.env.CLOUDINARY_KEY,
+    CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
+    CLOUDINARY_ENV: process.env.CLOUDINARY_ENV,
 }
