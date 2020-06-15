98e6b8d4e139abc87b82e78cb2d7bd977460dae4
luster02 <bombolti_02@hotmail.com>
2020-06-15 17:13:18 -0500

    [ADD]: route guard

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-
+import { verifyToken } from '../middlewares/auth.midd'
 export const UserRoutes = ({ UserController }: any) => {
     const router = Router()
 
-    router.get('/', UserController.getAll)
+    router.get('/', verifyToken, UserController.getAll)
     router.get('/:userId', UserController.get)
     router.patch('/:userId', UserController.update)
     router.delete('/:userId', UserController.delete)
