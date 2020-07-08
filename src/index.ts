d0f203b49d076d717198db127de976fd6a145133
luster02 <bombolti_02@hotmail.com>
2020-07-07 23:34:40 -0500

    [ADD]: useFindAndModify mongoose option

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ const server: any = container.resolve("app")
 const { MONGO_URI } = container.resolve("config")
 
 mongoose.set("useCreateIndex", true)
+mongoose.set('useFindAndModify', false)
 mongoose
-    .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true })
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(async () => {
         await server.start()
         console.log('database is connected')
