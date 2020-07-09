246ecac20ccade46dbc2551bf118ac870f70078e
luster02 <bombolti_02@hotmail.com>
2020-07-09 15:33:05 -0500

    [ADD]: token with less time

--- src/helpers/jwt.helper.ts
+++ src/helpers/jwt.helper.ts
@@ -4,3 +4,7 @@ import config from '../config'
 export function generateToken(user: any) {
     return sign({ user }, String(config.JWT_SECRET), { expiresIn: "4h" })
 }
+
+export function generateTokenResetPassword(user: any) {
+    return sign({ user }, String(config.JWT_SECRET), { expiresIn: "15m" })
+}
